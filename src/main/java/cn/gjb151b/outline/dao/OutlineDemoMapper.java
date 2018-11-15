8a35365d9c19486b982a1e042421a7d4c734e126
13366035571 <965718196@qq.com>
2018-11-15 15:48:54 +0800

    delete unuse db function
    Signed-off-by: 13366035571 <965718196@qq.com>

--- src/main/java/cn/gjb151b/outline/dao/OutlineDemoMapper.java
+++ src/main/java/cn/gjb151b/outline/dao/OutlineDemoMapper.java
@@ -6,13 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OutlineDemoMapper {
-    @Select("SELECT schema_1 FROM outline_demo WHERE outline_id = #{outlineID};")
-    String getJsonSchema(@Param("outlineID")int outlineID, @Param("pageNumber")int pageNumber);
-
-    @Select("SELECT data_1 FROM outline_demo WHERE outline_id = #{outlineID};")
-    String getJsonData(@Param("outlineID")int outlineID, @Param("pageNumber")int pageNumber);
-
-    // 这都是很不好的东西！但是很好用！直接无脑注入SQL语句
+    // 这都是很不安全的东西！但是很好用！直接无脑注入SQL语句
     void updateCol(@Param("outlineID")int outlineID, @Param("colName")String colName, @Param("data")String data);
 
     String selectCol(@Param("outlineID")int outlineID, @Param("colName")String colName);

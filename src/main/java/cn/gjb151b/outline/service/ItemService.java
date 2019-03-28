cd979e90b6af0c43a3a6f5c099dccb76286cd87a
fcupup <875894948@qq.com>
2019-03-28 17:34:39 +0800

    update

--- src/main/java/cn/gjb151b/outline/service/ItemService.java
+++ src/main/java/cn/gjb151b/outline/service/ItemService.java
@@ -8,6 +8,7 @@ import cn.gjb151b.outline.outlineDao.OutlineUserInfoMapper;
 import cn.gjb151b.outline.utils.BaseResponse;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.sun.tools.internal.xjc.outline.Outline;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
@@ -106,6 +107,17 @@ public class ItemService {
             String colName = "outline_data_"+i;
             manageSysOutlineMapper.updateItemCol(colName, "{}", devItemId);
         }
+        //将outline_data12, outline_data13, outline_schema_subsys_eqp,  outline_data_subsys_eqp字段初始化
+        manageSysOutlineMapper.updateItemCol("outline_schema_subsys_eqp", "{\"type\":\"object\",\"title\":\"分系统/设备信息\",\"properties\":{\"分系统或设备名称\":{\"type\":\"string\"},\"型号\":{\"type\":\"string\"},\"串号\":{\"type\":\"string\"},\"测试边界(m)\":{\"type\":\"string\"}}}", devItemId);
+        manageSysOutlineMapper.updateItemCol("outline_data_12", "{\"发射测试参数\":[{\"频率范围\":\"25Hz~1kHz\",\"6dB带宽(kHz)\":\"0.01\",\"视频带宽(kHz)\":\"\",\"驻留时间(s)\":\"0.15\",\"最小测量时间(模拟式测量接收机)\":\"0.015s/Hz\"},{\"频率范围\":\"1kHz~10kHz\",\"6dB带宽(kHz)\":\"0.1\",\"视频带宽(kHz)\":\"\",\"驻留时间(s)\":\"0.02\",\"最小测量时间(模拟式测量接收机)\":\"0.2s/kHz\"},{\"频率范围\":\"10kHz~150kHz\",\"6dB带宽(kHz)\":\"1\",\"视频带宽(kHz)\":\"\",\"驻留时间(s)\":\"0.02\",\"最小测量时间(模拟式测量接收机)\":\"0.02s/kHz\"},{\"频率范围\":\"150kHz~30MHz\",\"6dB带宽(kHz)\":\"10\",\"视频带宽(kHz)\":\"\",\"驻留时间(s)\":\"0.02\",\"最小测量时间(模拟式测量接收机)\":\"2s/MHz\"},{\"频率范围\":\"30MHz~1GHz\",\"6dB带宽(kHz)\":\"100\",\"视频带宽(kHz)\":\"\",\"驻留时间(s)\":\"0.02\",\"最小测量时间(模拟式测量接收机)\":\"0.2s/MHz\"},{\"频率范围\":\">1GHz\",\"6dB带宽(kHz)\":\"1000\",\"视频带宽(kHz)\":\"\",\"驻留时间(s)\":\"0.02\",\"最小测量时间(模拟式测量接收机)\":\"20s/GHz\"}]}", devItemId);
+        manageSysOutlineMapper.updateItemCol("outline_data_13", "{\"敏感度测试参数\":[{\"频率范围\":\"25Hz~1MHz\",\"模拟式扫描最大扫描速率(f0/s)\":\"0.0333\",\"步进式扫描最大步长(f0)\":\"0.05\",\"驻留时间\":\"\",\"调制类型(适用于CS114和RS103)\":\"脉冲\",\"重复频率(适用于CS114和RS103)\":\"1kHz\",\"占空比(适用于CS114和RS103)\":\"50%\"},{\"频率范围\":\"1MHz~30MHz\",\"模拟式扫描最大扫描速率(f0/s)\":\"0.00667\",\"步进式扫描最大步长(f0)\":\"0.01\",\"驻留时间\":\"\",\"调制类型(适用于CS114和RS103)\":\"脉冲\",\"重复频率(适用于CS114和RS103)\":\"1kHz\",\"占空比(适用于CS114和RS103)\":\"50%\"},{\"频率范围\":\"30MHz~1GHz\",\"模拟式扫描最大扫描速率(f0/s)\":\"0.00333\",\"步进式扫描最大步长(f0)\":\"0.005\",\"驻留时间\":\"\",\"调制类型(适用于CS114和RS103)\":\"脉冲\",\"重复频率(适用于CS114和RS103)\":\"1kHz\",\"占空比(适用于CS114和RS103)\":\"50%\"},{\"频率范围\":\"1GHz~40GHz\",\"模拟式扫描最大扫描速率(f0/s)\":\"0.00167\",\"步进式扫描最大步长(f0)\":\"0.0025\",\"驻留时间\":\"\",\"调制类型(适用于CS114和RS103)\":\"脉冲\",\"重复频率(适用于CS114和RS103)\":\"1kHz\",\"占空比(适用于CS114和RS103)\":\"50%\"}],\"敏感度试验波形参数\":{\"CS106\":[{\"上升时间(us)\":\"1.5(误差0.5us)\",\"下降时间(us)\":\"3.5(误差0.5us)\",\"脉冲宽度(us)\":\"5.5(误差22%)\"}],\"RS115\":[{\"上升时间(ns)\":\"1.8~2.8\",\"半脉宽(ns)\":\"23(误差5ns)\"}]}}", devItemId);
+        //outline_data_subsys_eqp 初始化
+        JSONObject eqpJson = new JSONObject();
+        eqpJson.put("分系统或设备名称", devSubsysEqpName);
+        eqpJson.put("型号", devSubsysEqpModel);
+        eqpJson.put("串号", devSubsysEqpNum);
+        eqpJson.put("测试边界(m)", "");
+        manageSysOutlineMapper.updateItemCol("outline_data_subsys_eqp", eqpJson.toJSONString(), devItemId);
         for (int i = 1001; i < 1006; i++) {
             String colName = "outline_data_"+i;
             manageSysOutlineMapper.updateItemCol(colName, "{}", devItemId);
